c12f04d52a0658dc3cf32353dc99c262187580df
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-03 19:49:53 -0400

    day12

--- src/day12/c1_doWhileLoop.java
+++ src/day12/c1_doWhileLoop.java
@@ -56,10 +56,10 @@ public class c1_doWhileLoop {
 
         int num1=0; // starting point
 
-        do {
+        do {//0 1 2 3 4 5 6 ..100
 
-            if (num1 % 2 ==0){
-                System.out.print(num1 + " , ");
+            if (num1 % 2 ==0){//0 2 4 6
+                System.out.print(num1 + " , "); //0 2 4 6
             }
             num1++; // iteration
 
