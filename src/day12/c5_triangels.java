9b2929dde96b7587f66dec7f70b729674cb80ea1
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-03 21:41:35 -0400

    day12

--- src/day12/c5_triangels.java
+++ src/day12/c5_triangels.java
@@ -97,6 +97,39 @@ public class c5_triangels {
 
          */
 
+        //asci table each letter has corresponding number
+        //you can make your start point either from asci number or asci letter
+
+        for (char ch='a' ; ch <= 'z' ; ch++){ // a b c d e ... z
+            //System.out.println(ch);
+
+            for (char ch2= 'a' ; ch2 <= ch ; ch2++){
+                System.out.print(ch2);
+                //if there is space between letters you need to add in the print
+            }
+
+            System.out.println();
+        }
+
+
+        //ch=a
+        //char ch2 = a ; ch2 <= a ; ch2++
+        //this will run one time
+        //a
+        //System.out.println();
+
+        //ch=b
+        //char ch2 = a ; ch2 <= b ; ch2++
+        //this will run 2 times
+        //ab
+        //System.out.println();
+
+        //ch=c
+        //char ch2 = a ; ch2 <= c ; ch2++
+        //this will run 3 times
+        //abc
+        //System.out.println();
+
 
 
 
