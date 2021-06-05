cfc8ee6d16b6f12213a3e3533850b90cbfaccae9
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-05 11:51:01 -0400

    day13

--- src/day13/c3_ArrayExamples.java
+++ src/day13/c3_ArrayExamples.java
@@ -26,5 +26,35 @@ public class c3_ArrayExamples {
         System.out.println();
 
 
+        //create a condition that will print min and max number from given array
+
+        int [] listOfNumber={10,0,100,-50,25,47,200};
+
+        //max 200
+        //min -50
+        //to have references
+        int max= listOfNumber[0]; //just assume this is max //10
+        int min= listOfNumber[0]; //just assume this is min //10
+
+        //then we will compare each element with max min variables and then reassign
+
+        for (int i=0;i < listOfNumber.length ; i++){// list0 =10 0 100 -50 ..200
+
+            if (listOfNumber[i] >= max){ //0 >=10  //100 >=10 // -50>=10
+                max = listOfNumber[i]; //this will reassign max number with listofnumbers i //10 //100 // 100 .. 200
+            }
+
+            if (listOfNumber[i] <= min){ //0 <= 10  //100 <=0  // -50 <=0
+                min = listOfNumber[i]; //0 //0 -50 ..
+            }
+
+        }
+
+        System.out.println("Max is "+max);
+        System.out.println("Min is "+min);
+
+
+
+
     }
 }
