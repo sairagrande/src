ee958b85390419f43837017f66ad085295a74036
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-15 19:37:46 -0400

    calculater

--- src/day17/c1_practice.java
+++ src/day17/c1_practice.java
@@ -2,5 +2,59 @@ package day17;
 
 public class c1_practice {
 
+    public static void main(String[] args) {
+        calculate(10,5,'*'); //10 * 5 == 50.0
 
+        int i=100;
+        int i2=20;
+        char ch='/';
+
+        calculate(i,i2,ch); //100 / 20 =5.0
+
+        calculate(120,2,'%');
+
+        calculate(20,30,'^'); //invalid operator : ^
+
+
+    }
+
+    // write a method that will take 3 parameters
+    // 2 numbers and one operator
+    // it will do the calculation
+
+    //ex : 20 10 /
+    //result 2
+
+    //ex 30 3 *
+    //result 90
+
+    public static void calculate(double number1 , double number2 , char operator){
+        // + - * / %
+        // invalid operator
+
+        switch (operator){
+            case '+' :
+                System.out.println(number1+number2);
+                break;
+            case '-':
+                System.out.println(number1-number2);
+                break;
+            case '*':
+                System.out.println(number1 * number2);
+                break;
+            case '/':
+                System.out.println(number1 / number2);
+                break;
+            case '%':
+                System.out.println(number1 % number2);
+                break;
+
+            default:
+                System.out.println("invalid operator : " + operator );
+
+        }
+
+
+
+    }
 }
