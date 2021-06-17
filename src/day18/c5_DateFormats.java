31dc0da5b7491a968beaa9acd00d8585320a306c
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-16 20:44:28 -0400

    dates

--- src/day18/c5_DateFormats.java
+++ src/day18/c5_DateFormats.java
@@ -1,8 +1,30 @@
 package day18;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class c5_DateFormats {
 
     public static void main(String[] args) {
+
+        DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("dd/MMM/yyyy");
+
+        LocalDate localDate1=LocalDate.now();
+        System.out.println(localDate1); //2021-06-16
+
+        System.out.println(localDate1.format(dateTimeFormatter)); //16/Jun/2021
+
+        DateTimeFormatter dateTimeFormatter2=DateTimeFormatter.ofPattern("E MMMM/dd/yy");
+
+        LocalDate localDate2=LocalDate.of(2010,10,05);
+
+        System.out.println(localDate2.format(dateTimeFormatter2));
+
+
+        //E : day of name
+        //MMM jan MMMM january
+        //yy :10  yyyy:2010
+
 
     }
 }
